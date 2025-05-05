6516b074053dca9fbe8baaa0828159d9ab4db872
RafaelSaCa <raffaellsaca@gmail.com>
2025-05-05 14:04:18 -0300

    LOGIN - acesso login armazenando token

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { AlertService } from '../../shared/services/alert.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit {
+
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private service: AuthService,
+              private router: Router,
+              private alert: AlertService) { }
+
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  login() {
+    const { email, senha } = this.form.value;
+
+    this.service.login({ email, senha }).subscribe({
+      next: (resposta) => {
+        localStorage.setItem('token', resposta.token);
+        this.router.navigate(['/usuarios']);
+      },
+      error: () => {
+        this.alert.error('Ops!','Ocorreu algum erro ao tentar fazer login!')
+      }
+    });
+  }
+
+}
---
+++ src/app/auth/model/payload-login.ts
@@ -0,0 +1,4 @@
+export interface PayLoadLogin {
+    email: string;
+    senha: string;
+}
