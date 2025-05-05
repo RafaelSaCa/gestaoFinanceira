9ca80fe9442bea6ecb735bd52bb9bcfdca0908b5
RafaelSaCa <raffaellsaca@gmail.com>
2025-05-05 14:05:43 -0300

    LOGIN - interceptors e guard (Evita adicionar o token em requisições que nao precisa de token de acesso

---
+++ src/app/auth/interceptor/auth.interceptor.ts
@@ -0,0 +1,22 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem('token');
+
+    // Evita adicionar o token em requisições para login/cadastro
+    const isAuthRequest = req.url.includes('/auth/login') || req.url.includes('/auth/register');
+
+    if (token && !isAuthRequest) {
+      const authReq = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`)
+      });
+      return next.handle(authReq);
+    }
+
+    return next.handle(req);
+  }
+}
