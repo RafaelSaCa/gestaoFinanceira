6ed9efcb82cc207569f356078fb8b9ce0d37c6ca
RafaelSaCa <raffaellsaca@gmail.com>
2025-05-05 14:09:46 -0300

    Roteamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './auth/guard/auth.guard';
+import { UsuariosComponent } from './usuario/usuarios/usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    },
+    {
+        path: '', redirectTo: 'auth/login', pathMatch: 'full'
+    },
+    {
+        path: 'usuarios',
+        component: UsuariosComponent,
+        canActivate: [AuthGuard]
+    }
+];
